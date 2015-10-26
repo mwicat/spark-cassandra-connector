aeed6325d826af26473547c7e1de87b7f832da68
Marek Wiewiorski <mwiewiorski@opera.com>
2015-10-26 15:24:36 +0100

    Print cql statement for token range together with bound variables on read timeout

--- spark-cassandra-connector/src/main/scala/com/datastax/spark/connector/cql/MultipleRetryPolicy.scala
+++ spark-cassandra-connector/src/main/scala/com/datastax/spark/connector/cql/MultipleRetryPolicy.scala
@@ -1,12 +1,20 @@
 package com.datastax.spark.connector.cql
 
+import scala.collection.JavaConversions._
+import scala.language.existentials
+
 import com.datastax.driver.core.policies.RetryPolicy
 import com.datastax.driver.core.policies.RetryPolicy.RetryDecision
 import com.datastax.driver.core.{ConsistencyLevel, Statement, WriteType}
 
+import org.apache.spark.Logging
+
+import com.datastax.driver.core._
+import com.datastax.spark.connector._
+
 /** Always retries with the same CL, constant number of times, regardless of circumstances */
 class MultipleRetryPolicy(maxRetryCount: Int, retryDelay: CassandraConnectorConf.RetryDelayConf)
-  extends RetryPolicy {
+  extends RetryPolicy with Logging {
 
   private def retryOrThrow(cl: ConsistencyLevel, nbRetry: Int): RetryDecision = {
     if (nbRetry < maxRetryCount) {
@@ -22,7 +30,22 @@ class MultipleRetryPolicy(maxRetryCount: Int, retryDelay: CassandraConnectorConf
 
   override def onReadTimeout(stmt: Statement, cl: ConsistencyLevel,
                              requiredResponses: Int, receivedResponses: Int,
-                             dataRetrieved: Boolean, nbRetry: Int) = retryOrThrow(cl, nbRetry)
+                             dataRetrieved: Boolean, nbRetry: Int) = {
+    if (nbRetry >= maxRetryCount) {
+      stmt match {
+        case bs: BoundStatement => {
+          val values = bs.preparedStatement().getVariables().zipWithIndex map { case (defn, i) =>
+            val rawValue = bs.getBytesUnsafe(i)
+            if (rawValue == null) null else defn.getType().deserialize(rawValue, ProtocolVersion.V3)
+          }
+          logError(s"Timeout statement $nbRetry " + bs.preparedStatement().getQueryString() + "[" + values.mkString(",") + "]")
+        }
+        case unknown => Unit
+      }
+    }
+
+    retryOrThrow(cl, nbRetry)
+  }
 
   override def onUnavailable(stmt: Statement, cl: ConsistencyLevel,
                              requiredReplica: Int, aliveReplica: Int, nbRetry: Int) = retryOrThrow(cl, nbRetry)
